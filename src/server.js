760795d5341f6d442998c1aa57953e1e0d6da2bd
SATHISHKUMAR <sathisharmy1572003@gmail.com>
2026-04-19 23:06:35 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -21,5 +21,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!', error: err.message });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+}
+
+module.exports = app;
+
